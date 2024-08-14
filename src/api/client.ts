a45f3ff64ee5b21f9cf87f39af81ac9dd30d9d4f
RiazanovKS <riazanovkiril99@gmail.com>
2024-08-15 00:41:41 +0300

    Feat
    Added token storage
    Added interceptors to send token with requests

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,18 @@
 import axios from 'axios';
+import { tokenStorage } from '@/storage/token';
 
 export const client = axios.create({
   baseURL: 'https://16.design.htmlacademy.pro/six-cities',
   timeout: 500,
 });
+
+client.interceptors.request.use(
+  (config) => {
+    const token = tokenStorage.get();
+    if (token) {
+      config.headers['X-Token'] = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
