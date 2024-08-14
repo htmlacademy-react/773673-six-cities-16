e0a16bbfb0fd3a0a44b8d2e612549c3e06ebb539
RiazanovKS <riazanovkiril99@gmail.com>
2024-08-15 00:48:48 +0300

    Feat
    Added user login logic to login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,23 @@
+import { AuthForm } from './ui/auth-form';
+
+const Login = () => (
+  <div className="page page--gray page--login">
+    <main className="page__main page__main--login">
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <AuthForm />
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <a className="locations__item-link" href="#">
+              <span>Amsterdam</span>
+            </a>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
+);
+
+export default Login;
