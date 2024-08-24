ba83b175b56d513cc7b186208a05eb5fb163c20d
RiazanovKS <riazanovkiril99@gmail.com>
2024-08-24 03:43:07 +0300

    Fixed build problems

--- src/pages/offer/ui/offer-info.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { OfferExtended } from '@/types/offer-extended';
-
-type Props = {
-  offer: OfferExtended;
-};
-
-export const OfferInfo: FC<Props> = ({ offer }) =>
